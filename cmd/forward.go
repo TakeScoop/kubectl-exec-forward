16fc0f7761c6f2d3d8b4e686e5dc0f0ecea0ee51
Ryan Wholey <ryan@takescoop.com>
2021-11-29 15:21:52 -0700

    Add command base

---
+++ cmd/forward.go
@@ -0,0 +1,170 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/phayes/freeport"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/takescoop/kubectl-port-forward-hooks/internal/command"
+	"github.com/takescoop/kubectl-port-forward-hooks/internal/kubernetes"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var annotation string = "local.service.kubernetes.io"
+
+// NewForwardCommand returns the command for forwarding to Kubernetes resources
+func NewForwardCommand() *cobra.Command {
+	overrides := &clientcmd.ConfigOverrides{}
+
+	cmd := &cobra.Command{
+		Use:   "kubectl port-forward-hooks svc [flags]",
+		Short: "Port forward to Kubernetes resources and execute commands found in annotations",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			flags := cmd.Flags()
+
+			client := kubernetes.New(overrides)
+
+			if err := client.Init(); err != nil {
+				return err
+			}
+
+			// TODO: handle Kubernetes resources other than service
+			service, err := client.GetService(ctx, args[0], &v1.GetOptions{})
+			if err != nil {
+				return err
+			}
+
+			preCommands, err := parseCommands(service.Annotations, fmt.Sprintf("%s/pre", annotation))
+			if err != nil {
+				return err
+			}
+
+			postCommands, err := parseCommands(service.Annotations, fmt.Sprintf("%s/post", annotation))
+			if err != nil {
+				return err
+			}
+
+			config := &command.Config{}
+
+			defaults, err := parseDefaults(service.Annotations)
+			if err != nil {
+				return err
+			}
+
+			for k, v := range defaults {
+				(*config)[k] = v
+			}
+
+			localPort, err := getLocalPort(flags, config)
+			if err != nil {
+				return err
+			}
+
+			user, _ := flags.GetString("username")
+			if user != "" {
+				(*config)["username"] = user
+			}
+
+			outputs := &command.Outputs{}
+
+			if err := preCommands.Execute(config, outputs); err != nil {
+				return err
+			}
+
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+			errChan := make(chan error)
+
+			handlers := &kubernetes.Handlers{
+				OnReady: func() {
+					if err := postCommands.Execute(config, outputs); err != nil {
+						if err != nil {
+							errChan <- err
+						}
+					}
+				},
+				OnStop: func() { <-sigChan },
+			}
+
+			if err = client.Forward(service, localPort, handlers); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	clientcmd.BindOverrideFlags(overrides, cmd.PersistentFlags(), clientcmd.RecommendedConfigOverrideFlags(""))
+	cmd.Flags().String("username", "", "Username")
+	cmd.Flags().Int("local-port", 0, "Local port")
+
+	return cmd
+}
+
+// Execute executes the forward command
+func Execute() {
+	cmd := NewForwardCommand()
+
+	cobra.CheckErr(cmd.Execute())
+}
+
+// parseCommands receives raw annotations in the form of a map and returns a list of parsed commands
+func parseCommands(annotations map[string]string, target string) (commands command.Commands, err error) {
+	annoCommands, ok := annotations[target]
+	if !ok {
+		return commands, nil
+	}
+
+	if err := json.Unmarshal([]byte(annoCommands), &commands); err != nil {
+		return nil, err
+	}
+
+	return commands, nil
+}
+
+// parseDefaults receives raw annotations, looks up the defaults key and returns them as a map
+func parseDefaults(annotations map[string]string) (defaults map[string]interface{}, err error) {
+	annoDefaults, ok := annotations[fmt.Sprintf("%s/defaults", annotation)]
+	if !ok {
+		return defaults, err
+	}
+
+	if err := json.Unmarshal([]byte(annoDefaults), &defaults); err != nil {
+		return nil, err
+	}
+
+	return defaults, err
+}
+
+// getLocalPort returns a port number, checking environment variables, then the defaults object, and finally a random open port
+func getLocalPort(flags *pflag.FlagSet, config *command.Config) (int, error) {
+	localPort, err := flags.GetInt("local-port")
+	if err != nil {
+		return 0, err
+	}
+
+	if localPort > 0 {
+		return localPort, nil
+	}
+
+	localPort, err = config.GetInt("localport")
+	if err != nil {
+		return 0, err
+	}
+
+	if localPort > 0 {
+		return localPort, nil
+	}
+
+	return freeport.GetFreePort()
+}
---
+++ internal/kubernetes/client.go
@@ -0,0 +1,66 @@
+package kubernetes
+
+import (
+	"context"
+
+	v1 "k8s.io/api/core/v1"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Handlers struct {
+	OnReady func()
+	OnStop  func()
+}
+
+type Client struct {
+	config    clientcmd.ClientConfig
+	client    *kubernetes.Clientset
+	namespace string
+}
+
+// Forward opens a connection to the passed service
+func (c Client) Forward(svc *v1.Service, localPort int, handlers *Handlers) error {
+	// TODO: implement the port forward
+	return nil
+}
+
+// New returns an uninitialized Kubernetes client
+func New(overrides *clientcmd.ConfigOverrides) *Client {
+	return &Client{
+		config: clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			clientcmd.NewDefaultClientConfigLoadingRules(),
+			overrides,
+		),
+	}
+}
+
+// Init initializes the calling Kubernetes client
+func (c *Client) Init() error {
+	restConfig, err := c.config.ClientConfig()
+	if err != nil {
+		return err
+	}
+
+	client, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return err
+	}
+
+	c.client = client
+
+	namespace, _, err := c.config.Namespace()
+	if err != nil {
+		return err
+	}
+
+	c.namespace = namespace
+
+	return nil
+}
+
+// GetService returns the service object denoted by the passed service name
+func (c Client) GetService(ctx context.Context, name string, options *v1meta.GetOptions) (*v1.Service, error) {
+	return c.client.CoreV1().Services(c.namespace).Get(ctx, name, *options)
+}
