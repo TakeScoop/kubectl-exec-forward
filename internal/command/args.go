eefcaf95b51d0a0f5725d91e2937e037d7f7feb9
Ryan Wholey <ryan@takescoop.com>
2021-12-16 16:10:08 -0700

    lint

--- internal/command/args.go
+++ internal/command/args.go
@@ -10,6 +10,7 @@ type Args map[string]string
 // parseArgs parses key value pairs from the passed annotations map, adds any overrides passed and returns a new args map.
 func parseArgs(annotations map[string]string, overrides map[string]string) (*Args, error) {
 	args := Args{}
+
 	v, ok := annotations[ArgsAnnotation]
 	if ok {
 		if err := json.Unmarshal([]byte(v), &args); err != nil {
