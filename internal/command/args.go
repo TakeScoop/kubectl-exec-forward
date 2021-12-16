93d1f4beef17949a164a5331f9ac6e0815e6ae58
Ryan Wholey <ryan@takescoop.com>
2021-12-16 16:58:08 -0700

    fix: Overrides not applied when annotations do not exist (#24)

--- internal/command/args.go
+++ internal/command/args.go
@@ -8,24 +8,19 @@ import (
 type Args map[string]string
 
 // parseArgs parses key value pairs from the passed annotations map, adds any overrides passed and returns a new args map.
-func parseArgs(annotations map[string]string, overrides map[string]string) (args *Args, err error) {
+func parseArgs(annotations map[string]string, overrides map[string]string) (*Args, error) {
+	args := Args{}
+
 	v, ok := annotations[ArgsAnnotation]
-	if !ok {
-		return args, err
+	if ok {
+		if err := json.Unmarshal([]byte(v), &args); err != nil {
+			return nil, err
+		}
 	}
 
-	if err := json.Unmarshal([]byte(v), &args); err != nil {
-		return nil, err
-	}
-
-	args.addOverrides(overrides)
-
-	return args, err
-}
-
-// addOverrides adds the passed key value pairs to the calling args map.
-func (a *Args) addOverrides(overrides map[string]string) {
 	for k, v := range overrides {
-		(*a)[k] = v
+		args[k] = v
 	}
+
+	return &args, nil
 }
