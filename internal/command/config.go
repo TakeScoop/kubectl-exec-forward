19508c0bae73a82a1eafbaba37e11cba2569a443
Ryan Wholey <ryan@takescoop.com>
2021-12-17 11:25:53 -0700

    Set local ports on the config (#25)

--- internal/command/config.go
+++ internal/command/config.go
@@ -2,6 +2,6 @@ package command
 
 // Config stores configuration which is used to construct the tunnel as well as passed to the hook commands.
 type Config struct {
-	LocalPort int
-	Verbose   bool
+	LocalPorts []int
+	Verbose    bool
 }
