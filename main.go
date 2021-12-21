e4d55a05ef17ea7d5ad672b65f3810691f952cc7
Ryan Wholey <ryan@takescoop.com>
2021-12-21 14:10:31 -0700

    Replace empty version string with "dev"

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 // version will be replaced with the Git tag version at build time during release.
-var version string
+var version string = "dev"
 
 func main() {
 	cmd.Execute(version)
