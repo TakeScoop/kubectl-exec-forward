12943014ccdbbd0876eca173d3bb2e7f8be5be38
Ryan Wholey <ryan@takescoop.com>
2022-01-05 14:56:16 -0700

    Fix incorrect LocalPort when port 0 requested (#58)
    * Allow zero port
    
    * Read local port from ready connection

--- internal/forwarder/forward.go
+++ internal/forwarder/forward.go
@@ -7,8 +7,14 @@ import (
 	"k8s.io/client-go/transport/spdy"
 )
 
+// Connection stores port-forwarding information for an open connection.
+type Connection struct {
+	Local  int
+	Remote int
+}
+
 // Forward creates a port-forwarding connection to the target noted by the ForwardConfig object.
-func (c Client) Forward(config *Config, readyChan chan struct{}, stopChan chan struct{}) error {
+func (c Client) Forward(config *Config, readyChan chan Connection, stopChan chan struct{}) error {
 	transport, upgrader, err := spdy.RoundTripperFor(c.restConfig)
 	if err != nil {
 		return err
@@ -25,10 +31,36 @@ func (c Client) Forward(config *Config, readyChan chan struct{}, stopChan chan s
 
 	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", url)
 
-	fw, err := portforward.New(dialer, []string{config.Port}, stopChan, readyChan, c.streams.Out, c.streams.ErrOut)
+	openChan := make(chan struct{})
+	errChan := make(chan error)
+
+	fw, err := portforward.New(dialer, []string{config.Port}, stopChan, openChan, c.streams.Out, c.streams.ErrOut)
 	if err != nil {
 		return err
 	}
 
-	return fw.ForwardPorts()
+	go func() {
+		if err := fw.ForwardPorts(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	for {
+		select {
+		case <-openChan:
+			ports, err := fw.GetPorts()
+			if err != nil {
+				return err
+			}
+
+			readyChan <- Connection{
+				Local:  int(ports[0].Local),
+				Remote: int(ports[0].Remote),
+			}
+
+			return nil
+		case err := <-errChan:
+			return err
+		}
+	}
 }
