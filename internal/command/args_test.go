f15fb46b4b3933276701f688434a031d41551d80
Ben Drucker <bvdrucker@gmail.com>
2022-01-06 16:06:37 -0800

    convert to table tests

--- internal/command/args_test.go
+++ internal/command/args_test.go
@@ -4,55 +4,59 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestParseArgs(t *testing.T) {
-	t.Run("Parse basic args", func(t *testing.T) {
-		annotations := map[string]string{
-			PreAnnotation:  `{}`,
-			ArgsAnnotation: `{"username":"foo","schema":"https"}`,
-		}
+func TestParseArgsFromAnnotations(t *testing.T) {
+	t.Parallel()
 
-		args, err := ParseArgsFromAnnotations(annotations, map[string]string{})
-		assert.NoError(t, err)
+	cases := []struct {
+		name        string
+		annotations map[string]string
+		overrides   map[string]string
+		expected    Args
+	}{
+		{
+			name:        "basic",
+			annotations: map[string]string{ArgsAnnotation: `{"username":"foo","schema":"https"}`},
+			expected: Args{
+				"username": "foo",
+				"schema":   "https",
+			},
+		},
+		{
+			name:        "overrides",
+			annotations: map[string]string{ArgsAnnotation: `{"username":"foo","schema":"https"}`},
+			overrides:   map[string]string{"username": "bar"},
+			expected: Args{
+				"username": "bar",
+				"schema":   "https",
+			},
+		},
+		{
+			name: "overrides without default in annotation",
+			annotations: map[string]string{
+				ArgsAnnotation: "{}",
+			},
+			overrides: map[string]string{"username": "bar"},
+			expected:  Args{"username": "bar"},
+		},
+		{
+			name:     "no annotation",
+			expected: Args{},
+		},
+	}
 
-		expected := Args{
-			"username": "foo",
-			"schema":   "https",
-		}
+	for _, tc := range cases {
+		tc := tc
 
-		assert.Equal(t, &expected, args)
-	})
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	t.Run("Parse with overrides", func(t *testing.T) {
-		annotations := map[string]string{
-			PreAnnotation:  `{}`,
-			ArgsAnnotation: `{"username":"foo","schema":"https"}`,
-		}
+			actual, err := ParseArgsFromAnnotations(tc.annotations, tc.overrides)
+			require.NoError(t, err)
 
-		args, err := ParseArgsFromAnnotations(annotations, map[string]string{
-			"username": "bar",
+			assert.Equal(t, &tc.expected, actual)
 		})
-		assert.NoError(t, err)
-
-		expected := Args{
-			"username": "bar",
-			"schema":   "https",
-		}
-
-		assert.Equal(t, &expected, args)
-	})
-
-	t.Run("Add overrides with no args annotation", func(t *testing.T) {
-		args, err := ParseArgsFromAnnotations(map[string]string{}, map[string]string{
-			"username": "bar",
-		})
-		assert.NoError(t, err)
-
-		expected := Args{
-			"username": "bar",
-		}
-
-		assert.Equal(t, &expected, args)
-	})
+	}
 }
