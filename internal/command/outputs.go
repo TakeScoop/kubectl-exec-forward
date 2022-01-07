d6a6e5353be408bd0603929dcd06fbf617fb2e31
Ben Drucker <bvdrucker@gmail.com>
2022-01-07 12:20:51 -0800

    Add test for Append()

--- internal/command/outputs.go
+++ internal/command/outputs.go
@@ -3,12 +3,14 @@ package command
 // Outputs is a collection of command outputs keyed by ID.
 type Outputs map[string]string
 
-// append copies the existing output, appending the new output, and returns the new, extended outputs.
+// Append copies the existing output, appending the new output, and returns the new, extended outputs.
 func (o Outputs) Append(id string, output string) Outputs {
 	outputs := Outputs{}
+
 	for k, v := range o {
 		outputs[k] = v
 	}
+
 	outputs[id] = output
 	return outputs
 }
